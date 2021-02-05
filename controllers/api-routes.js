9322b19fc73a1439e27808aa35638bd92eb8811b
Anthony Loredo <anthonyloredo5@gmail.com>
2021-02-05 11:30:37 -0500

    Added test route for api and fixed node_modules folder

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -2,38 +2,64 @@
 let db = require("../models");
 let passport = require("../config/passport");
 
-module.exports = function(app) {
+
+module.exports = function (app) {
+  //Api test route
+  app.get("/apiTest", function (req, res) {
+    const request = require('request');
+
+    const options = {
+      method: 'GET',
+      url: 'https://target1.p.rapidapi.com/auto-complete',
+      qs: { q: 'macbook air' },
+      headers: {
+        'x-rapidapi-key': '060fa7bd32mshd4d14b256c582fbp173924jsn92ccfae51f8e',
+        'x-rapidapi-host': 'target1.p.rapidapi.com',
+        useQueryString: true
+      }
+    };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      res.json(console.log("Body", body));
+      res.json(console.log("Response", response));
+    });
+  });
+
+
+
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     res.json(req.user);
   });
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
-  app.post("/api/signup", function(req, res) {
+  app.post("/api/signup", function (req, res) {
     db.User.create({
       email: req.body.email,
       password: req.body.password
     })
-      .then(function() {
+      .then(function () {
         res.redirect(307, "/api/login");
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
 
   // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
+  app.get("/api/user_data", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
