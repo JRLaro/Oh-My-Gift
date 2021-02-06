16825516042f8148b7659ae67ba497d0ea499f00
J.Laro <joselarojr@gmail.com>
2021-02-06 14:38:23 -0500

    added keyframes to the logo - animated

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
